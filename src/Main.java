baa46376266fdb21c2b6e5180da28b993d82c46f
Konstantin S <skg@genialconsult.ru>
2023-10-16 12:14:39 +0300

    исправил логику

--- src/Main.java
+++ src/Main.java
@@ -8,45 +8,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //int[] mas = readNumberFromF("INPUT.TXT");
 
         Scanner scan = new Scanner(new File("INPUT.TXT"));
         int n = scan.nextInt();
-        int d = scan.nextInt();
+        final int d = scan.nextInt(); //значение, которое не подлежит изменению
         int[] mas = new int[n];
         for (int i = 0; i < mas.length; i++) {
             mas[i] = scan.nextInt();
         }
         System.out.println(Arrays.toString(mas));
-        System.out.println("d = "+d);
-        System.out.println("n = "+n);
-        //int n = mas.length;
-        //int d = mas[0];
-        int k = 0; //кол-во байдарок
+        System.out.println("Грузоподъёмность d = "+d);
+        System.out.println("Всего человек = "+n);
 
+        int k = 0; //кол-во байдарок
+        Arrays.sort(mas);
+
+        System.out.println(Arrays.toString(mas));
 
         if (!((n <= 1) || (d <= 1) || (n >= 15000) || (d >= 15000))) {
 
-            for (int i = 1; i < mas.length - 1; i++) {
-                if ((mas[i] + mas[i + 1]) < d) {
-                    k++;
-                    System.out.println("Байдарок Нужно " + k);
-                    String str = Integer.toString(k); // преобразование числа в строку
-                    String outputFName = "OUTPUT.TXT";
-                    Files.writeString(Path.of(outputFName), str);
+            for (int i = 0; i < n - 1; i++) {
+                int i_light = i;
+                int i_heavy = n-1;
+                if ((mas[i_light] + mas[i_heavy]) >= d) {
+                    k++; i_light++;
+                    i_heavy--;
                 }
+                else{
+                    k++; i_heavy--;
+                }
+
             }
+            System.out.println("Байдарок нужно " + k);
+            //String str = Integer.toString(k); // преобразование числа в строку
+            String str = String.valueOf(k);    // преобразование числа в строку
+            String outputFName = "OUTPUT.TXT";
+            Files.writeString(Path.of(outputFName), str);
         }
         else
         System.out.println("Конец истории");
     }
-    private static int[] readNumberFromF (String fileName) throws FileNotFoundException {
-            Scanner scan = new Scanner(new File(fileName));
-            int n = scan.nextInt();
-            int[] mas = new int[n];
-            for (int i = 0; i < mas.length; i++) {
-                mas[i] = scan.nextInt();
-            }
-            return mas;
-        }
+
 }
